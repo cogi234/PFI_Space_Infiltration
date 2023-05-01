91a9081188dbdcf86d9de5947037d0ef389d37b1
cogi234 <colinbougie@gmail.com>
2023-05-01 15:09:57 -0400

    Movement works somewhat

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float walkingSpeed;
+    [SerializeField] float walkingHeight;
+
+    Vector3 velocity;
+    public Vector3 Velocity { get => velocity; }
 
     Rigidbody rb;
 
@@ -13,9 +17,52 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
     }
 
+    private void Update()
+    {
+        //Si le collider est kinematique, c'est qu'on est entrain de marcher sur quelque chose.
+        //Sinon, on est entrain de flotter dans le vide
+        if (rb.isKinematic)
+        {
+        } else
+        {
+
+        }
+    }
+
     private void FixedUpdate()
     {
-        rb.velocity = new Vector3(Input.GetAxis("Horizontal") * walkingSpeed, 0, Input.GetAxis("Vertical") * walkingSpeed);
+        //Si le collider est kinematique, c'est qu'on est entrain de marcher sur quelque chose.
+        //Sinon, on est entrain de flotter dans le vide
+        if (rb.isKinematic)
+        {
+            //On change notre vitesse selon les touches de mouvement
+            velocity = new Vector3(Input.GetAxis("Horizontal") * walkingSpeed, 0, Input.GetAxis("Vertical") * walkingSpeed).normalized;
+
+            //On bouge selon notre vitesse
+            transform.Translate(velocity * Time.fixedDeltaTime, Space.Self);
+
+            //On utilise un SphereCast pour s'orienter selon notre plancher
+            RaycastHit hit;
+            if (Physics.SphereCast(transform.position, 0.25f, -transform.up, out hit, walkingHeight * 2, ~LayerMask.GetMask("Player")))
+            {
+                //Si la difference entre la hauteur desiree et la hauteur en ce moment est trop haute ou basse, on ajuste
+                if (Mathf.Abs(walkingHeight - hit.distance) > 0.01f)
+                    transform.Translate(Vector3.up * (walkingHeight - hit.distance));
+
+                transform.rotation = Quaternion.LookRotation(transform.forward, hit.normal);
+                Debug.Log(hit.distance);
+
+            } else
+            {
+                //Si on ne trouve pas de plancher, on flotte dans le vide
+                rb.isKinematic = false;
+                rb.velocity = velocity;
+                velocity = Vector3.zero;
+            }
+        } else
+        {
+
+        }
     }
 
 }
