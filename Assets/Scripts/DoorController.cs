024a2c171650dbc86fb75bb66e625b72c0cf1692
cogi234 <colinbougie@gmail.com>
2023-05-10 15:48:10 -0400

    Doors

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+    [SerializeField] Transform leftDoor, rightDoor;
+    [SerializeField] Vector3 leftMovement, rightMovement;
+    [SerializeField] float speed = 1;
+    [SerializeField] float openRadius;
+
+    float openness = 0;
+    Vector3 leftClosed, leftOpen, rightClosed, rightOpen;
+
+    private void Awake()
+    {
+        leftClosed = leftDoor.localPosition;
+        rightClosed = rightDoor.localPosition;
+
+        leftOpen = leftClosed + leftMovement;
+        rightOpen = rightClosed + rightMovement;
+    }
+
+    private void Update()
+    {
+        //Si le joueur est assez proche, on ouvre
+        if (Physics.CheckSphere(transform.position, openRadius, LayerMask.GetMask("Player")))
+            openness = Mathf.Clamp(openness + (Time.deltaTime * speed), 0, 1);
+        else //Sinon, on ferme
+            openness = Mathf.Clamp(openness - (Time.deltaTime * speed), 0, 1);
+
+        //On applique ca sur les portes
+        leftDoor.localPosition = Vector3.Lerp(leftClosed, leftOpen, openness);
+        rightDoor.localPosition = Vector3.Lerp(rightClosed, rightOpen, openness);
+    }
+}
