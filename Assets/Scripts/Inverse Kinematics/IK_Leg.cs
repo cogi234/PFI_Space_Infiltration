9f976f94b02c7e37d5d5777702d9449720266e3f
EmileGrenon <egrenon@bell.net>
2023-05-10 20:00:12 -0400

    Revert "tourrel"
    This reverts commit 87c23d9e0ce6fa7d986a1167fa9b8ec4f43ac49d, reversing
    changes made to 0429374e8834c8b02f2877f939e5893bf912f983.

--- Assets/Scripts/Inverse Kinematics/IK_Leg.cs
+++ Assets/Scripts/Inverse Kinematics/IK_Leg.cs
@@ -72,7 +72,6 @@ public class IK_Leg : MonoBehaviour
             if (!touchingTarget && Vector3.Distance(ankle.position, target) <= targetTouchTreshold)
             {
                 touchingTarget = true;
-                stepSound.pitch = Random.Range(0.75f, 1.25f);
                 stepSound.Play();
             }
 
