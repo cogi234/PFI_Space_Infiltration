4f06c09159028264446f3b51a8f2abd7e087dac0
cogi234 <colinbougie@gmail.com>
2023-05-18 12:49:09 -0400

    Bullet Spark

--- Assets/Scripts/BulletComponent.cs
+++ Assets/Scripts/BulletComponent.cs
@@ -10,12 +10,16 @@ public class BulletComponent : MonoBehaviour
     /// </summary>
     public int damage = 1;
 
+    [SerializeField] GameObject sparkPrefab;
+
     private void OnCollisionEnter(Collision collision)
     {
         //On essaie de prendre le HealthComponent de l'autre objet pour l'endommager
         HealthComponent health;
         if (collision.gameObject.TryGetComponent<HealthComponent>(out health))
             health.TakeDamage(damage);
+        //On fait apparaitre une etincelle
+        GameObject.Instantiate(sparkPrefab, collision.contacts[0].point, Quaternion.LookRotation(collision.contacts[0].normal, transform.up));
         //On desactive la balle
         GetComponent<Rigidbody>().velocity = Vector3.zero;
         GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
